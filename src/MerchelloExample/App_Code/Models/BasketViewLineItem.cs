0e2fd153156e319de3f071894588a5040e3260aa
Sydney Cole <sydney.cole@gmail.com>
2014-05-14 21:06:52 -0700

    Initial commit. Work in progress.

---
+++ src/MerchelloExample/App_Code/Models/BasketViewLineItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Site.Models
+{
+    public class BasketViewLineItem
+    {
+        // ItemCacheLineItem Key (basket item key)
+        public Guid Key { get; set; }
+
+        // This is not part of Merchello - This is a customization so we can provide a link to the product page
+        // eg. The Umbraco content Id
+        public int ContentId { get; set; }
+
+        /// <summary>
+        /// This is the name from the <see cref="IProductVariant"/>
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// This is a hack ...
+        /// </summary>
+        public string Attributes { get; set; }
+
+        /// <summary>
+        /// The sku of the <see cref="IProductVariant"/>
+        /// </summary>
+        public string Sku { get; set; }
+
+        /// <summary>
+        /// The price (can be overrided) of the <see cref="IProductVariant"/>
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// The quantity to purchase
+        /// </summary>
+        public int Quantity { get; set; }
+    }
+}
